8cfafdfc43b4acdece979b0a59295b9fde099749
KhadzhievaMariyam <khadzhiieva2000@mail.ru>
2024-09-29 22:09:13 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+name = "Mariyam"
+print ("Name:" ,name)
+age = "24"
+print ("Age:" ,age)
+b=24
+c=1
+newage = b+c
+print ("New age:" , newage)
+isstudent = "True"
+print ("Is  student:", isstudent)
